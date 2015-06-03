8fae16b1c3182134ce777d7e481b6298ce3bba8f
Jonny Ervine <jervine@users.noreply.github.com>
2015-06-03 21:48:43 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+##
+## Start up script for Plex Media Server on CentOS docker container
+##
+
+## Initialise any variables being called:
+# Set the correct timezone
+TZ=${TZ:-UTC}
+setup=/config/plexmediaserver/.setup
+PMS-RPM=$PMS-RPM
+
+if [ ! -f "${setup}" ]; then
+  rm -f /etc/localtime
+  cp /usr/share/zoneinfo/$TZ /etc/localtime
+  yum install -y $PMS-RPM
+  yum clean all
+  touch $setup
+fi
+
+## Start up Plex Media Server daemon via supervisord
+/usr/bin/supervisord -n -c /etc/supervisord.conf
