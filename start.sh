bc76bce225e7c42ebe78da32da8a939c1b9e9543
Jonny Ervine <jervine@users.noreply.github.com>
2015-07-19 11:11:29 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -21,5 +21,14 @@ mount --bind /data/plex/media /var/lib/plexmediaserver/Library/Application Suppo
 mount --bind /data/plex/metadata /var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Metadata
 mount --bind /data/plex/sync /var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Cache/Transcode/Sync
 
+PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR=/var/lib/plexmediaserver/Library/Application\ Support
+Environment=PLEX_MEDIA_SERVER_HOME=/usr/lib/plexmediaserver
+Environment=PLEX_MEDIA_SERVER_MAX_PLUGIN_PROCS=6
+Environment=PLEX_MEDIA_SERVER_TMPDIR=/tmp
+Environment=LD_LIBRARY_PATH=/usr/lib/plexmediaserver
+Environment=LC_ALL=en_US.UTF-8
+Environment=LANG=en_US.UTF-8
+/usr/bin/test -d "${PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR}" || /bin/mkdir -p "${PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR}"
+
 ## Start up Plex Media Server daemon via supervisord
 /usr/bin/supervisord -n -c /etc/supervisord.conf
